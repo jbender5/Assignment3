639a6a1edae0a246bc575be6412b7f3940c413f2
jbender5 <joshbender@uchicago.edu>
2020-03-10 23:22:58 -0500

    Commit on Master

---
+++ example_pkg/base.py
@@ -0,0 +1,18 @@
+def hello():
+    return "Hello!"
+
+class salad():
+    def write():
+        #should receive a path
+        # should receive a list of salad items
+        #should receive a list of number of salad items
+        #should rite a file with item_00.salad
+
+    # input path, [tomato, lettuce], [2,3]
+    #xxx/tomato_00.salad
+    #xxx/tomato_00.salad
+
+    def read():
+        #read the .salad files
+        #return a dictonary with sald items and how many there are
+
